96e8ecd9b40a7d0ee15b7db1903bb8161bd2e440
annekauf <151649251+annekauf@users.noreply.github.com>
2024-05-07 16:36:35 +0200

    two more tests
    African American Review
    close #75
    see #25
    close #78

--- cypress/e2e/nested.cy.js
+++ cypress/e2e/nested.cy.js
@@ -1,6 +1,6 @@
 describe('Nested Work', () => {
     // see #8  
-    describe('African American Review', () => {
+    describe('African American Review -All', () => {
         // Title search and online access false to limit noise
         // check if exact match first list item is the journal parent and subsequent entries are child works
         // a more elaborate test would compare the value of '.record-number' within the same family
@@ -9,13 +9,62 @@ describe('Nested Work', () => {
                 url: '/Results',
                 qs: {
                     lookfor: 'African American Review',
-                    type: 'Title',
+                    type: 'allFields',
                     "filter[]": '~remote_bool:"false"'
                 }
             })
         })
 
         // see #25
+        it.skip('should appear before child work', () => {
+            cy.get('#result0')
+                .find('.media-type')
+                .contains(' Zeitschrift (gedruckt) ')
+            cy.get('#result1')
+                .find('.media-type')
+                .contains(' Band einer Zeitschrift/Zeitung ')
+        })
+    })
+	
+	describe('African American Review -Journal', () => {
+        // check if exact match first list item is the journal parent and subsequent entries are child works
+		// see #75
+    
+        beforeEach(() => {
+            cy.visit({
+                url: '/Results',
+                qs: {
+                    lookfor: 'African American Review',
+                    type: 'JournalSearch',
+                    "filter[]": '~remote_bool:"false"'
+                }
+            })
+        })
+
+        it('should appear before child work', () => {
+            cy.get('#result0')
+                .find('.media-type')
+                .contains(' Zeitschrift (gedruckt) ')
+            cy.get('#result1')
+                .find('.media-type')
+                .contains(' Band einer Zeitschrift/Zeitung ')
+        })
+    })
+	
+	describe('African American Review -Title', () => {
+        // check if exact match first list item is the journal parent and subsequent entries are child works
+    
+        beforeEach(() => {
+            cy.visit({
+                url: '/Results',
+                qs: {
+                    lookfor: 'African American Review',
+                    type: 'title',
+                    "filter[]": '~remote_bool:"false"'
+                }
+            })
+        })
+
         it.skip('should appear before child work', () => {
             cy.get('#result0')
                 .find('.media-type')
