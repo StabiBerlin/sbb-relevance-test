d8da18914e247d3594750cc4d0201b4b6d95f3c5
annekauf <151649251+annekauf@users.noreply.github.com>
2024-09-25 11:01:35 +0200

    Added 2 Tests African
    Added identical Search but once searchfield "AllFields" and once searchfield "JournalTitle" to nested.cy.js
    
    see #8

--- cypress/e2e/nested.cy.js
+++ cypress/e2e/nested.cy.js
@@ -8,7 +8,7 @@ describe('Nested Work', () => {
             cy.visit({
                 url: '/Results',
                 qs: {
-                    lookfor: 'African American Review',
+                    lookfor: 'African American review',
                     type: 'Title',
                     "filter[]": '~remote_bool:"false"'
                 }
@@ -16,13 +16,62 @@ describe('Nested Work', () => {
         })
 
         // see #25
-        it.skip('should appear before child work', () => {
+        it.only('should appear before child work', () => {
             cy.get('#result0')
                 .find('.media-type')
                 .contains(' Zeitschrift (gedruckt) ')
             cy.get('#result1')
                 .find('.media-type')
                 .contains(' Band einer Zeitschrift/Zeitung ')
+				
+describe('African American Review', () => {
+         // check if exact match first list item is the journal parent and subsequent entries are child works
+        // a more elaborate test would compare the value of '.record-number' within the same family
+        beforeEach(() => {
+            cy.visit({
+                url: '/Results',
+                qs: {
+                    lookfor: 'African American review',
+                    type: 'AllFields',
+                    "filter[]": '~remote_bool:"false"'
+                }
+            })
+        })
+
+        // see #25
+        it.only('should appear before child work', () => {
+            cy.get('#result0')
+                .find('.media-type')
+                .contains(' Zeitschrift (gedruckt) ')
+            cy.get('#result1')
+                .find('.media-type')
+                .contains(' Band einer Zeitschrift/Zeitung ')
+				
+describe('African American Review', () => {
+        // Title search and online access false to limit noise
+        // check if exact match first list item is the journal parent and subsequent entries are child works
+        // a more elaborate test would compare the value of '.record-number' within the same family
+        beforeEach(() => {
+            cy.visit({
+                url: '/Results',
+                qs: {
+                    lookfor: 'African American review',
+                    type: 'Title',
+                    "filter[]": '~remote_bool:"false"'
+                }
+            })
+        })
+
+        // see #25
+        it.only('should appear before child work', () => {
+            cy.get('#result0')
+                .find('.media-type')
+                .contains(' Zeitschrift (gedruckt) ')
+            cy.get('#result1')
+                .find('.media-type')
+                .contains(' Band einer Zeitschrift/Zeitung ')
+
+
         })
     })
 
